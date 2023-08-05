4c9fddb0ce23c2ef94af3b917ec93e884604f667
danipishinin <danipishinin>
2023-08-05 14:33:12 -0300

    enunciado

---
+++ 1038-snack.js
@@ -0,0 +1,22 @@
+/**
+ * Using the following table, write a program that reads a code and the amount of an item. 
+ * After, print the value to pay. This is a very simple program with the only intention of practice of selection commands.
+ * 
+---------------------------------------
+CODE        SPECIFICATION       PRICE
+---------------------------------------
+1           CACHORRO QUENTE     R$4.00
+2           X-SALADA            R$4.50
+3           X-BACON             R$5.00
+4           TORRADA SIMPLES     R$2.00
+5           REFRIGERANTE        R$1.50
+---------------------------------------
+
+Input
+The input file contains two integer numbers X and Y. 
+X is the product code and Y is the quantity of this item according to the above table.
+
+Output
+The output must be a message "Total: R$ " followed by the total value to be paid, with 2 digits after the decimal point.
+ */
+
