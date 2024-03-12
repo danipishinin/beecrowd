c1c953eea54ed145b10c1d86402371e2851b810e
daniele pishinin <danipishinin@gmail.com>
2024-03-12 19:52:17 -0300

    month

---
+++ 1052-month.js
@@ -0,0 +1,48 @@
+/*Read an integer number between 1 and 12, including. Corresponding to this number, 
+you must print the month of the year, in english, with the first letter in uppercase.
+
+Input
+The input contains only an integer number.
+
+Output
+Print the name of the month according to the input number, with the first letter in uppercase.
+*/
+
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+var month = parseInt(lines.shift());
+
+function getMonth() {
+    switch (month) {
+        case 1:
+            return 'January';
+        case 2:
+            return 'February';
+        case 3:
+            return 'March';
+        case 4:
+            return 'April';
+        case 5:
+            return 'May';
+        case 6:
+            return 'June';
+        case 7:
+            return 'July';
+        case 8:
+            return 'August';
+        case 9:
+            return 'September';
+        case 10:
+            return 'October';
+        case 11:
+            return 'November';
+        case 12:
+            return 'December';
+        default:
+            break;
+    }
+}
+
+const result = getMonth();
+console.log(result)
