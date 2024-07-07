dd21bfa38788d40f69ff9edd2edcc51529a9bf55
Daniele Pishinin::nn <danipishinin@gmail.com>
2024-07-07 10:56:59 -0300

    sequence ij 2

---
+++ javascript/1096-sequence-ij-2.js
@@ -0,0 +1,31 @@
+/**
+ * Make a program that prints the sequence like the following exemple.
+
+Input
+This problem doesn't have input.
+
+Output
+Print the sequence like the example below.
+
+Input Sample	Output Sample
+                I=1 J=7
+                I=1 J=6
+                I=1 J=5
+                I=3 J=7
+                I=3 J=6
+                I=3 J=5
+                ...
+                I=9 J=7
+                I=9 J=6
+                I=9 J=5
+*
+**/
+
+
+let j = [7,6,5];
+
+for(let i = 1; i <= 9; i += 2){
+    for(let k = 0; k < j.length; k++){
+        console.log(`I=${i} J=${j[k]}`);
+    }
+}
