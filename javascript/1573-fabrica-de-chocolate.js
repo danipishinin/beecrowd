b6e54c680e6aeeffa244036e1e5a8b7ba3c6bfd5
Daniele Pishinin::nn <danipishinin@gmail.com>
2026-06-05 08:51:17 -0300

    fabrica de chocolate

---
+++ javascript/1573-fabrica-de-chocolate.js
@@ -0,0 +1,28 @@
+/**
+ * Uma fábrica produz barras de chocolates no formato de paralelepípedos e de cubos, com o mesmo volume. Porém, como a máquina que produz os chocolates em formato de cubo está apresentando alguns problemas, os donos da fábrica pediram a sua ajuda para resolver este problema.
+
+Sua tarefa é, dadas as dimensões das arestas do chocolate em formato de paralelepípedo, dizer qual é o tamanho que a aresta em formato de cubo deve ter.
+
+Entrada
+A entrada contém vários casos de teste. A primeira linha de cada caso de teste contém três inteiros A, B e C (1 ≤ A, B, C ≤ 103), indicando os tamanhos das arestas do chocolate em formato de paralelepípedo. A entrada termina quando A = B = C = 0, e não deve ser processado.
+
+Saída
+Para cada entrada, você deve imprimir um único inteiro que deve ser truncado,representando o tamanho da aresta do chocolate em forma de cubo.
+
+Exemplo de Entrada	Exemplo de Saída
+170 867 253         334
+452 378 368         397
+5 6 7               5
+2 4 5               3
+0 0 0
+ */
+
+var input = require('fs').readFileSync('stdin', 'utf-8');
+var lines = input.split('\n');
+
+lines.some((line) => {
+    const [A,B,C] = line.split(' ').map(Number);
+    if(A === B &&  B === C && C === 0) return true;
+    const rcubic = Math.cbrt(A * B * C);
+    console.log(Math.trunc(rcubic))
+})
